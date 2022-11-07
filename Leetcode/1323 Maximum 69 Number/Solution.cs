20d742f66fe587b068956a0cd27fa24fd43f3a3b
Karuption <pederson26@gmail.com>
2022-11-07 16:54:31 -0600

    Accepted alternate regex method

--- Leetcode/1323 Maximum 69 Number/Solution.cs
+++ Leetcode/1323 Maximum 69 Number/Solution.cs
@@ -1,8 +1,15 @@
-﻿namespace Leetcode._1323_Maximum_69_Number;
+﻿using System.Text.RegularExpressions;
+
+namespace Leetcode._1323_Maximum_69_Number;
 
 public class Solution {
+    //Alternate, regex method
+    //public int Maximum69Number(int num) {
+    //    Regex regex = new("6", RegexOptions.Compiled);
+    //    return int.Parse(regex.Replace(num.ToString(), "9", 1));
+    //}
+
     public int Maximum69Number(int num) {
-        //could do a regex replace aswell.
         var number = num.ToString().ToList();
         var firstSix = number.IndexOf('6');
 
