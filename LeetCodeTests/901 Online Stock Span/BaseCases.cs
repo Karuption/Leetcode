b1a410c3ee6f4eb36c25aab967841360e035dc00
Karuption <pederson26@gmail.com>
2022-11-09 01:10:44 -0600

    901 Online Stack Span Solution

---
+++ LeetCodeTests/901 Online Stock Span/BaseCases.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode._901_Online_Stock_Span;
+
+namespace LeetCodeTests._901_Online_Stock_Span;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { 100, 80, 60, 70, 60, 75, 85 }, new[] { 1, 1, 1, 2, 1, 4, 6 })]
+    private void BaseCase(int[] input, int[] expected) {
+        var sut = new StockSpanner();
+        var actual = new List<int>();
+
+        foreach (var i in input)
+            actual.Add(sut.Next(i));
+
+        Assert.NotEmpty(actual);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/901 Online Stock Span/StockSpanner.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode._901_Online_Stock_Span;
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
+public class StockSpanner {
+    private readonly Stack<KeyValuePair<int, int>> stack = new(); //price, previousStreak
+
+    public int Next(int price) {
+        var previousLessThan = 1; //since we include itself in the value
+
+        //While there is an entry that is less than the current entry, pop the stack
+        //when we pop the stack, take the value (previous streak) and add to the current streak.
+        while (stack.Count > 0 && price >= stack.Peek().Key) previousLessThan += stack.Pop().Value;
+
+        stack.Push(new KeyValuePair<int, int>(price, previousLessThan));
+
+        return previousLessThan;
+    }
+}
