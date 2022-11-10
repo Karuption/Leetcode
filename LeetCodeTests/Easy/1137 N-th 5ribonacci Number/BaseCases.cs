fd8c0f7029d47b898ee2ea9e7a627cc13eb7f432
Karuption <pederson26@gmail.com>
2022-11-10 13:11:35 -0600

    Solution to 1137 N-th Tribonacci Number.

---
+++ LeetCodeTests/Easy/1137 N-th 5ribonacci Number/BaseCases.cs
@@ -0,0 +1,16 @@
+﻿using Leetcode.Easy._1137_N_th_Tribonacci_Number;
+
+namespace LeetCodeTests.Easy._1137_N_th_5ribonacci_Number;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(4, 4)]
+    [InlineData(25, 1389537)]
+    public void BaseCase(int n, int expected) {
+        var sut = new Solution();
+
+        var result = sut.Tribonacci(n);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Leetcode/Easy/1137 N-th Tribonacci Number/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode.Easy._1137_N_th_Tribonacci_Number;
+
+public class Solution {
+    public int Tribonacci(int n) {
+        if (n < 1) return 0;
+        if (n < 3) return 1;
+
+        int num1 = 2, num2 = 1, num3 = 1;
+
+        for (var i = 0; i < n - 3; i++) (num1, num2, num3) = (num1 + num2 + num3, num1, num2);
+
+        return num1;
+    }
+}
