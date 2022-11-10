756a12e5f16baf03cfc52a93d6d3fb1fdbd09008
Karuption <pederson26@gmail.com>
2022-11-10 12:52:36 -0600

    Solution to 509 Fibonacci Number using Dynamic Programming.

---
+++ LeetCodeTests/509 Fibonacci Number/BaseCases.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode._509_Fibonacci_Number;
+
+namespace LeetCodeTests._509_Fibonacci_Number;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 3)]
+    [InlineData(0, 0)]
+    public void BaseCase(int n, int expected) {
+        var sut = new Solution();
+
+        var result = sut.Fib(n);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Leetcode/509 Fibonacci Number/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode._509_Fibonacci_Number;
+
+public class Solution {
+    //using DP
+    public int Fib(int n) {
+        if (n < 1) return 0;
+        if (n < 3) return 1;
+        int num1 = 1, num2 = 1;
+
+        for (var i = 0; i < n - 2; i++) (num1, num2) = (num1 + num2, num1);
+
+        return num1;
+    }
+}
