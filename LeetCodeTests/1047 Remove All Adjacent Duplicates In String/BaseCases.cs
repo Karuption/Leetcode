ce5894bf29656b74f86abb44bcf1d87ceef2e57a
Karuption <pederson26@gmail.com>
2022-11-09 18:49:53 -0600

    Two solutions for 1047. Remove All Adjacent Duplicates In String

---
+++ LeetCodeTests/1047 Remove All Adjacent Duplicates In String/BaseCases.cs
@@ -0,0 +1,16 @@
+﻿using Leetcode._1047_Remove_All_Adjacent_Duplicates_In_String;
+
+namespace LeetCodeTests._1047_Remove_All_Adjacent_Duplicates_In_String;
+
+public class BaseCases {
+    [Theory]
+    [InlineData("abbaca", "ca")]
+    [InlineData("azxxzy", "ay")]
+    public void BaseCase(string input, string expected) {
+        var sut = new Solution();
+
+        var result = sut.RemoveDuplicates(input);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Leetcode/1047 Remove All Adjacent Duplicates In String/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Leetcode._1047_Remove_All_Adjacent_Duplicates_In_String;
+
+public class Solution {
+    //beats 86.73% in runtime
+    //beats 88.63% in memory
+    //public string RemoveDuplicates(ReadOnlySpan<char> s) {
+    //    Stack<char> stack = new();
+
+
+    //    for (var i = 0; i < s.Length; i++) {
+    //        if (stack.Count == 0 || !s[i].Equals(stack.Peek()))
+    //            stack.Push(s[i]);
+    //        else
+    //            stack.Pop();
+    //    }
+
+    //    return new string(stack.Reverse().ToArray());
+    //}
+
+    //beats 81.52% in runtime
+    //beats 88.63% in memory
+    public string RemoveDuplicates(ReadOnlySpan<char> s) {
+        StringBuilder sb = new(s.Length);
+
+
+        for (var i = 0; i < s.Length; i++) {
+            if (sb.Length == 0 || !s[i].Equals(sb[sb.Length - 1]))
+                sb.Append(s[i]);
+            else
+                sb.Remove(sb.Length - 1, 1);
+        }
+
+        return sb.ToString();
+    }
+}
+
