8b98f4c29f330fc991df31cb9d7fb254ab2c0d24
Karuption <pederson26@gmail.com>
2022-11-11 12:59:24 -0600

    746 min cost Climbing stairs solution

--- LeetCodeTests/Easy/746 Min Cost Climbing Stairs/BaseCases.cs
+++ LeetCodeTests/Easy/746 Min Cost Climbing Stairs/BaseCases.cs
@@ -7,6 +7,7 @@ public class BaseCases {
     [InlineData(new[] { 10, 15, 20 }, 15)]
     [InlineData(new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
     [InlineData(new[] { 0, 2, 2, 1 }, 2)]
+    [InlineData(new[] { 1, 100 }, 1)]
     public void BaseCase(int[] input, int expected) {
         Solution sut = new();
 
