82bcb70dfbbf02601544fb842bd147e4691180bf
Karuption <pederson26@gmail.com>
2022-11-11 23:34:08 -0600

    Solution to 295 Find Median from Data Stream solution

---
+++ LeetCodeTests/Hard/295 Find Median from Data Stream/BaseCases.cs
@@ -0,0 +1,44 @@
+﻿using Leetcode.Hard._295_Find_Median_from_Data_Stream;
+
+namespace LeetCodeTests.Hard._295_Find_Median_from_Data_Stream;
+
+public class BaseCases {
+    [Fact]
+    public void BaseCase() {
+        MedianFinder sut = new();
+
+        var actual = new double[3];
+
+        sut.AddNum(1);
+        sut.AddNum(2);
+
+        Assert.Equal(1.5, sut.FindMedian());
+
+        sut.AddNum(3);
+
+        Assert.Equal(2.0d, sut.FindMedian());
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        MedianFinder sut = new();
+
+        var actual = new double[5];
+
+        sut.AddNum(-1);
+        Assert.Equal(-1d, sut.FindMedian());
+        sut.AddNum(-2);
+
+        Assert.Equal(-1.5d, sut.FindMedian());
+
+        sut.AddNum(-3);
+        Assert.Equal(-2d, sut.FindMedian());
+
+        sut.AddNum(-4);
+        Assert.Equal(-2.5d, sut.FindMedian());
+
+        sut.AddNum(-5);
+
+        Assert.Equal(-3d, sut.FindMedian());
+    }
+}
---
+++ Leetcode/Hard/295 Find Median from Data Stream/MedianFinder.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode.Hard._295_Find_Median_from_Data_Stream;
+
+public class MedianFinder {
+    private readonly List<int> _data = new();
+
+    public void AddNum(int num) {
+        var left = 0;
+        var right = _data.Count;
+        int middle;
+        int compare;
+        while (left < right) {
+            middle = left + (right - left) / 2;
+            compare = _data[middle].CompareTo(num);
+            if (compare > 0) {
+                right = middle;
+            }
+            else if (compare < 0) {
+                left = middle + 1;
+            }
+            else {
+                _data.Insert(middle, num);
+                return;
+            }
+        }
+
+        _data.Insert(left, num);
+    }
+
+    public double FindMedian() {
+        if (_data.Count % 2 == 0)
+            return (_data[(_data.Count - 1) / 2] + _data[_data.Count / 2]) / 2d;
+        return _data[(_data.Count - 1) / 2];
+    }
+}
