da504e9cdb48f89208ebb62f54b3665fe0d1a1d8
Karuption <pederson26@gmail.com>
2022-11-13 16:36:23 -0600

    213 house robber 2 solution

---
+++ LeetCodeTests/Medium/213 House Robber 2/BaseCases.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode.Medium._213_House_Robber_2;
+
+namespace LeetCodeTests.Medium._213_House_Robber_2;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { 2, 3, 2 }, 3)]
+    [InlineData(new[] { 1, 2, 3, 1 }, 4)]
+    [InlineData(new[] { 1, 2, 3 }, 3)]
+    [InlineData(new[] { 200, 3, 140, 20, 10 }, 340)]
+    public void BaseCase(int[] input, int expected) {
+        Solution sut = new();
+
+        var actual = sut.Rob(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/213 House Robber 2/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Medium._213_House_Robber_2;
+
+public class Solution {
+    public int Rob(int[] nums) {
+        if (nums.Length < 4) return nums.Max();
+
+        var num1l = 0;
+        var num1r = 0;
+        var num2r = 0;
+        var num2l = 0;
+        var currentl = 0;
+        var currentr = 0;
+
+        for (var i = 1; i < nums.Length; i++) {
+            currentr = Math.Max(num2r, nums[i] + num1r);
+            (num1r, num2r) = (num2r, currentr);
+
+            currentl = Math.Max(num2l, nums[i - 1] + num1l);
+            (num1l, num2l) = (num2l, currentl);
+        }
+
+        return Math.Max(currentl, currentr);
+    }
+}
