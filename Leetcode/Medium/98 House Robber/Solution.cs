0ad24493685a30ba69d92bbc102ac7296091c578
Karuption <pederson26@gmail.com>
2022-11-13 15:47:02 -0600

    98 house robber solution

--- Leetcode/Medium/98 House Robber/Solution.cs
+++ Leetcode/Medium/98 House Robber/Solution.cs
@@ -1,14 +1,16 @@
 ﻿namespace Leetcode.Medium._98_House_Robber;
 
 public class Solution {
-    private Dictionary<int, int> dict;
-
     public int Rob(ReadOnlySpan<int> nums) {
-        //dict = new(nums.Length);
-        int[] cost = { 0, 0 };
+        var num1 = 0; // lag variables
+        var num2 = 0;
+        var current = 0;
 
-        for (var i = 0; i < nums.Length; i++) cost[i % 2] += nums[i];
+        for (var i = 0; i < nums.Length; i++) {
+            current = Math.Max(num2, num1 + nums[i]);
+            (num1, num2) = (num2, current);
+        }
 
-        return Math.Max(cost[0], cost[1]);
+        return current;
     }
 }
