92ef3c3b2a05dbfb02c9e2f743d4c542ecf20aab
Karuption <pederson26@gmail.com>
2022-11-13 17:56:23 -0600

    Solution for 740 Delete and Earn. Solution inspired from problem 213 Robbing houses

---
+++ LeetCodeTests/Medium/740 Delete and Earn/BaseCases.cs
@@ -0,0 +1,15 @@
+﻿using Leetcode.Medium._740_Delete_and_Earn;
+
+namespace LeetCodeTests.Medium._740_Delete_and_Earn;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { 3, 4, 2 }, 6)]
+    [InlineData(new[] { 2, 2, 3, 3, 3, 4 }, 9)]
+    public void BaseCase(int[] input, int expected) {
+        Solution sut = new();
+        var actual = sut.DeleteAndEarn(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/740 Delete and Earn/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace Leetcode.Medium._740_Delete_and_Earn;
+
+public class Solution {
+    // Memory optimized, blend counting sort(ish) with robbing houses (medium 213)
+    // only allocating O(1)+O(m) where m is distinct values of n
+    public int DeleteAndEarn(ReadOnlySpan<int> nums) {
+        SortedDictionary<int, int> dict = new();
+        var min = nums[0];
+        var max = nums[0];
+
+        for (var i = 0; i < nums.Length; ++i) {
+            if (dict.TryGetValue(nums[i], out var temp)) {
+                dict[nums[i]] = temp + nums[i];
+            }
+            else {
+                dict.Add(nums[i], nums[i]);
+                if (nums[i] > max)
+                    max = nums[i];
+                else if (nums[i] < min)
+                    min = nums[i];
+            }
+        }
+
+        var num1 = 0;
+        var num2 = 0;
+        var current = 0;
+        for (var i = min; i < max + 1; ++i) {
+            if (dict.TryGetValue(i, out var item)) {
+                current = Math.Max(item + num1, num2);
+                num1 = num2;
+                num2 = current;
+            }
+            else {
+                num1 = num2 = Math.Max(num1, num2);
+            }
+        }
+
+        return current;
+    }
+}
