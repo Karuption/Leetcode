e3deb666c791e79809274fc13027794f3e79f5df
Karuption <pederson26@gmail.com>
2022-11-14 00:36:29 -0600

    Solution for 947 Most Stones Removed with Same Row or Column

---
+++ LeetCodeTests/Medium/947 Most Stones Removed with Same Row or Column/BaseCases.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode.Medium._947_Most_Stones_Removed_with_Same_Row_or_Column;
+
+namespace LeetCodeTests.Medium._947_Most_Stones_Removed_with_Same_Row_or_Column;
+
+public class BaseCases {
+    private readonly int[][] arr =
+        { new[] { 0, 0 }, new[] { 0, 1 }, new[] { 1, 0 }, new[] { 1, 2 }, new[] { 2, 1 }, new[] { 2, 2 } };
+
+    private readonly int[][] arr1 = { new[] { 0, 0 }, new[] { 0, 2 }, new[] { 1, 1 }, new[] { 2, 0 }, new[] { 2, 2 } };
+
+    [Fact]
+    public void BaseCase() {
+        Solution sut = new();
+
+        var actual = sut.RemoveStones(arr);
+
+        Assert.Equal(5, actual);
+    }
+
+    [Fact]
+    public void BaseCase1() {
+        Solution sut = new();
+
+        var actual = sut.RemoveStones(arr1);
+
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        Solution sut = new();
+
+        var actual = sut.RemoveStones(new[] { new[] { 0, 0 } });
+
+        Assert.Equal(0, actual);
+    }
+}
---
+++ Leetcode/Medium/947 Most Stones Removed with Same Row or Column/Solution.cs
@@ -0,0 +1,55 @@
+﻿namespace Leetcode.Medium._947_Most_Stones_Removed_with_Same_Row_or_Column;
+
+public class Solution {
+    public int RemoveStones(int[][] stones) {
+        UnionFind uf = new(stones);
+
+        foreach (var stone in stones) uf.Union(stone[0] + 1, -(stone[1] + 1));
+
+        return stones.Length - uf.count;
+    }
+}
+
+public class UnionFind {
+    private readonly Dictionary<int, int> mapping = new();
+    private readonly Dictionary<int, int> rank = new();
+    public int count;
+
+    public UnionFind(int[][] stones) {
+        foreach (var stone in stones) {
+            var x = stone[0] + 1;
+            var y = -(stone[1] + 1);
+            mapping[x] = x;
+            rank[x] = 0;
+            mapping[y] = y;
+            rank[y] = 0;
+        }
+
+        count = mapping.Count;
+    }
+
+    public int find(int node) {
+        if (mapping[node] != node) return find(mapping[node]);
+        return mapping[node];
+    }
+
+    public void Union(int x, int y) {
+        var xSet = find(x);
+        var ySet = find(y);
+        if (xSet == ySet) return;
+        count--;
+
+        if (rank.GetValueOrDefault(xSet) > rank.GetValueOrDefault(ySet)) {
+            mapping[ySet] = mapping[xSet];
+        }
+
+        else if (rank.GetValueOrDefault(xSet) < rank.GetValueOrDefault(ySet)) {
+            mapping[xSet] = mapping[ySet];
+        }
+
+        else {
+            mapping[xSet] = mapping[ySet];
+            rank[xSet] = rank.GetValueOrDefault(ySet) + 1;
+        }
+    }
+}
