1b95288c65a0b72693a41c5e12e2dbacef1a5fcc
Karuption <pederson26@gmail.com>
2022-11-16 22:30:17 -0600

    223 Rectangle area first solution

---
+++ LeetCodeTests/Medium/223 Rectangle Area/BaseCases.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode.Medium._223_Rectangle_Area;
+
+namespace LeetCodeTests.Medium._223_Rectangle_Area;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(-3, 0, 3, 4, 0, -1, 9, 2, 45)]
+    [InlineData(-2, -2, 2, 2, -2, -2, 2, 2, 16)]
+    [InlineData(-2, -2, 2, 2, 3, 3, 4, 4, 17)]
+    [InlineData(2, 2, -2, -2, -3, -3, -4, -4, 17)]
+    public void BaseCase(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int expected) {
+        Solution sut = new();
+
+        var actual = sut.ComputeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
+
+        Assert.Equal(expected, actual);
+    }
+}
--- Leetcode/Easy/374 Guess Number Higher or Lower/Solution.cs
+++ Leetcode/Easy/374 Guess Number Higher or Lower/Solution.cs
@@ -1,9 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Leetcode.Easy._374_Guess_Number_Higher_or_Lower;
 
-namespace Leetcode.Easy._374_Guess_Number_Higher_or_Lower;
-internal class Solution {
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ * 1 if num is lower than the picked number
+ * otherwise return 0
+ * int guess(int num);
+ */
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        var lowerBound = 1;
+        var upperBound = n;
+        var _guess = 0;
+        var gOutput = -1; //-1 is chosen to not interfere with the loop checking the output
+
+        while (gOutput != 0) {
+            _guess = (upperBound - lowerBound) / 2 + lowerBound;
+
+            gOutput = guess(_guess);
+
+            if (gOutput == 1)
+                lowerBound = _guess + 1;
+            else
+                upperBound = _guess - 1;
+        }
+
+        return _guess;
+    }
+}
+
+//This class is provided by Leetcode internals.
+//This is only present to remove errors
+public abstract class GuessGame {
+    public int guess(int x) {
+        return 1;
+    }
 }
