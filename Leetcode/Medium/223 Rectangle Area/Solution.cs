eed08917a4723dda199e16303cf9229507a5ebe1
Karuption <pederson26@gmail.com>
2022-11-16 22:31:43 -0600

    Second solution accepted. Removed Math.Abs and used better comparison logic to check for overlap.

--- Leetcode/Medium/223 Rectangle Area/Solution.cs
+++ Leetcode/Medium/223 Rectangle Area/Solution.cs
@@ -2,20 +2,17 @@
 
 public class Solution {
     public int ComputeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        var total = 0;
-
         var bx = Math.Max(ax1, bx1);
         var by = Math.Max(ay1, by1);
         var tx = Math.Min(ax2, bx2);
         var ty = Math.Min(ay2, by2);
 
-        var isInverted = tx - bx < 0 || ty - by < 0;
+        int total = (ax2 - ax1) * (ay2 - ay1) +
+                    (bx2 - bx1) * (by2 - by1);
 
-        if (!isInverted) total -= Math.Abs((tx - bx) * (ty - by));
-
-        total += Math.Abs((ax2 - ax1) * (ay2 - ay1)) +
-                 Math.Abs((bx2 - bx1) * (by2 - by1));
-
+        if (tx > bx && ty > by) {
+            return total - (tx - bx) * (ty - by);
+        }
         return total;
     }
 }
