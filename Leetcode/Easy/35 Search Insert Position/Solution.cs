76b32889d972e7ace339a1d0956038d3db07e2f9
Karuption <pederson26@gmail.com>
2022-11-26 23:29:15 -0600

    Simple Binary search solution

---
+++ Leetcode/Easy/35 Search Insert Position/Solution.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy._35_Search_Insert_Position;
+internal class Solution {
+}
