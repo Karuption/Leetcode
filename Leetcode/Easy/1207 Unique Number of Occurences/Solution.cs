12ecd5ca9b6b94457e8b027c6ef1d24bbfc0365d
Karuption <pederson26@gmail.com>
2022-11-29 22:45:17 -0600

    Quick solution to 1207.

---
+++ Leetcode/Easy/1207 Unique Number of Occurences/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode.Easy._1207_Unique_Number_of_Occurences;
+
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) {
+        Dictionary<int, int> Occurences = new();
+
+        for (var i = 0; i < arr.Length; i++) {
+            if (Occurences.TryGetValue(arr[i], out var temp))
+                Occurences[arr[i]]++;
+            else
+                Occurences[arr[i]] = 1;
+        }
+
+        return Occurences.Values.Count == Occurences.Values.Distinct().Count();
+    }
+}
--- Leetcode/Easy/35 Search Insert Position/Solution.cs
+++ Leetcode/Easy/35 Search Insert Position/Solution.cs
@@ -1,9 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Leetcode.Easy._35_Search_Insert_Position;
 
-namespace Leetcode.Easy._35_Search_Insert_Position;
-internal class Solution {
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        var left = 0;
+        var right = nums.Length;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return right;
+    }
 }
