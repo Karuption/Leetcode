d52a487223c68a5d87bd4cd4af914ac99a663809
Karuption <pederson26@gmail.com>
2022-12-17 21:54:21 -0600

    completed 739. Daily Temperatures

---
+++ LeetCodeTests/Medium/739 Daily Temperatures/BaseCases.cs
@@ -0,0 +1,17 @@
+﻿using Leetcode.Medium._739_Daily_Temperatures;
+
+namespace LeetCodeTests.Medium._739_Daily_Temperatures;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { 73, 74, 75, 71, 69, 72, 76, 73 }, new[] { 1, 1, 4, 2, 1, 1, 0, 0 })]
+    [InlineData(new[] { 30, 40, 50, 60 }, new[] { 1, 1, 1, 0 })]
+    [InlineData(new[] { 30, 60, 90 }, new[] { 1, 1, 0 })]
+    public void Test_Daily_Temperatures(int[] input, int[] expected) {
+        Solution sut = new();
+
+        var actual = sut.DailyTemperatures(input);
+
+        Assert.Equal(expected, actual, EqualityComparer<int>.Default);
+    }
+}
---
+++ Leetcode/Medium/739 Daily Temperatures/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.Medium._739_Daily_Temperatures;
+
+public class Solution {
+    private int[] output;
+
+    public int[] DailyTemperatures(int[] temperatures) {
+        Stack<(int, int)> stack = new(temperatures.Length);
+        output = new int[temperatures.Length];
+
+        for (var i = 0; i < temperatures.Length; i++) {
+            while (stack.TryPeek(out var res) && res.Item1 < temperatures[i])
+                output[res.Item2] = i - stack.Pop().Item2;
+
+            stack.Push((temperatures[i], i));
+        }
+
+        return output;
+    }
+}
