e603560dd09293404557efe587e386a29eaf7156
Karuption <pederson26@gmail.com>
2022-12-19 21:34:37 -0600

    Solution for 1971 find if path exists in graph

---
+++ LeetCodeTests/Easy/1971 Find if Path Exists in Graph/BaseCases.cs
@@ -0,0 +1,58 @@
+﻿using Leetcode.Easy._1971._Find_if_Path_Exists_in_Graph;
+
+namespace LeetCodeTests.Easy._1971_Find_if_Path_Exists_in_Graph;
+
+public class BaseCases {
+    [Fact]
+    public void BaseCase() {
+        var edges = new[] {
+            new[] { 0, 1 }, new[] { 1, 2 }, new[] { 2, 0 }
+        };
+
+        var source = 0;
+        var destination = 2;
+
+        Solution sut = new();
+
+        var actual = sut.ValidPath(3, edges, source, destination);
+
+        Assert.Equal(true, actual);
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        var Solution = new Solution();
+
+        var edges = new[] {
+            new[] { 0, 1 }, new[] { 0, 2 }, new[] { 3, 5 }, new[] { 5, 4 }, new[] { 4, 3 }
+        };
+
+        var source = 0;
+        var destination = 5;
+
+        Solution sut = new();
+
+        var actual = sut.ValidPath(6, edges, source, destination);
+
+        Assert.Equal(false, actual);
+    }
+
+    [Fact]
+    public void BaseCase3() {
+        var Solution = new Solution();
+
+        var edges = new[] {
+            new[] { 4, 3 }, new[] { 1, 4 }, new[] { 4, 8 }, new[] { 1, 7 }, new[] { 6, 4 }, new[] { 4, 2 },
+            new[] { 7, 4 }, new[] { 4, 0 }, new[] { 0, 9 }, new[] { 5, 4 }
+        };
+
+        var source = 5;
+        var destination = 9;
+
+        Solution sut = new();
+
+        var actual = sut.ValidPath(10, edges, source, destination);
+
+        Assert.Equal(true, actual);
+    }
+}
---
+++ Leetcode/Easy/1971. Find if Path Exists in Graph/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode.Easy._1971._Find_if_Path_Exists_in_Graph;
+
+public class Solution {
+    public bool ValidPath(int n, int[][] edges, int source, int destination) {
+        if (source == destination)
+            return true;
+
+        var map = new HashSet<int>[n];
+
+        foreach (var edge in edges) {
+            map[edge[0]] ??= new HashSet<int>();
+            map[edge[0]].Add(edge[1]);
+
+            map[edge[1]] ??= new HashSet<int>();
+            map[edge[1]].Add(edge[0]);
+        }
+
+        Queue<int> q = new();
+        q.Enqueue(source);
+
+        int current;
+        HashSet<int> visited = new();
+
+        while (q.Count > 0) {
+            current = q.Dequeue();
+            if (current == destination)
+                return true;
+
+            visited.Add(current);
+
+            foreach (var item in map[current]) {
+                if (!q.Contains(item) && !visited.Contains(item))
+                    q.Enqueue(item);
+            }
+        }
+
+        return false;
+    }
+}
