deed472dcd881cfcc6fc1dfb028b9bb85271cfdd
Karuption <pederson26@gmail.com>
2022-12-21 05:18:55 -0600

    Solved 886 Possible Bipartition

---
+++ LeetCodeTests/Medium/886 Possible Bipartition/BaseCases.cs
@@ -0,0 +1,62 @@
+﻿using Leetcode.Medium._886_Possible_Bipartition;
+
+namespace LeetCodeTests.Medium._886_Possible_Bipartition;
+
+public class BaseCases {
+    [Fact]
+    public void BaseCase1() {
+        var expected = true;
+        var n = 4;
+
+        Solution sut = new();
+
+        var dislikes = new[] { new[] { 1, 2 }, new[] { 1, 3 }, new[] { 2, 4 } };
+
+        var actual = sut.PossibleBipartition(n, dislikes);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        var expected = false;
+        var n = 3;
+
+        Solution sut = new();
+
+        var dislikes = new[] { new[] { 1, 2 }, new[] { 1, 3 }, new[] { 2, 3 } };
+
+        var actual = sut.PossibleBipartition(n, dislikes);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BaseCase3() {
+        var expected = false;
+        var n = 5;
+
+        Solution sut = new();
+
+        var dislikes = new[] { new[] { 1, 2 }, new[] { 2, 3 }, new[] { 3, 4 }, new[] { 4, 5 }, new[] { 1, 5 } };
+
+        var actual = sut.PossibleBipartition(n, dislikes);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BaseCase4() {
+        var expected = true;
+        var n = 10;
+
+        Solution sut = new();
+
+        var dislikes = new[]
+            { new[] { 1, 2 }, new[] { 3, 4 }, new[] { 5, 6 }, new[] { 6, 7 }, new[] { 8, 9 }, new[] { 7, 8 } };
+
+        var actual = sut.PossibleBipartition(n, dislikes);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/886 Possible Bipartition/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Leetcode.Medium._886_Possible_Bipartition;
+
+public class Solution {
+    private static readonly HashSet<int> EmptySet = new();
+    private int[] _colors;
+    private HashSet<int>?[] _set;
+
+    public bool PossibleBipartition(int n, int[][] dislikes) {
+        _set = new HashSet<int>?[n];
+        _colors = new int[n];
+
+        foreach (var dislike in dislikes) {
+            _set[--dislike[0]] ??= new HashSet<int>();
+            _set[dislike[0]]!.Add(--dislike[1]);
+            //sneaky decrement on both elements to shift range [1,n] -> [0,n-1]
+
+            _set[dislike[1]] ??= new HashSet<int>();
+            _set[dislike[1]]!.Add(dislike[0]);
+        }
+
+        for (var i = 0; i < n; i++) {
+            if (_colors[i] != 0) continue;
+
+            if (!Color(i, 1))
+                return false;
+        }
+
+        return true;
+    }
+
+    private bool Color(int i, int color) {
+        Queue<int> q = new();
+
+        q.Enqueue(i);
+        int current;
+        while (q.Count > 0) {
+            current = q.Dequeue();
+
+            foreach (var item in _set[current] ?? EmptySet) {
+                if (_colors[item] == 0) {
+                    q.Enqueue(item);
+                    _colors[item] = _colors[current] % 2 + 1;
+                } else if (_colors[item] == _colors[current]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
