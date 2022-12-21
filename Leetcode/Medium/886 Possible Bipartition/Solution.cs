2adc3e67e52c4c450b4c6941d26ad65486ca3f0e
Karuption <pederson26@gmail.com>
2022-12-21 05:34:26 -0600

    refactoring for solution to 886

--- Leetcode/Medium/886 Possible Bipartition/Solution.cs
+++ Leetcode/Medium/886 Possible Bipartition/Solution.cs
@@ -29,20 +29,13 @@ public class Solution {
     }
 
     private bool Color(int i, int color) {
-        Queue<int> q = new();
-
-        q.Enqueue(i);
-        int current;
-        while (q.Count > 0) {
-            current = q.Dequeue();
-
-            foreach (var item in _set[current] ?? EmptySet) {
-                if (_colors[item] == 0) {
-                    q.Enqueue(item);
-                    _colors[item] = _colors[current] % 2 + 1;
-                } else if (_colors[item] == _colors[current]) {
-                    return false;
-                }
+        foreach (var item in _set[i] ?? EmptySet) {
+            if (_colors[item] == default) {
+                _colors[item] = _colors[i] % 2 + 1; //set the opposite color
+                if (!Color(item, _colors[i]))
+                    return false; //propagate any recursion failures
+            } else if (_colors[item] == _colors[i]) {
+                return false;
             }
         }
 
