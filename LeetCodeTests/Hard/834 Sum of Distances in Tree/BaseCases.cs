8f1fa293511726a1903542ae4c845d0a1b01d1ff
Karuption <pederson26@gmail.com>
2022-12-22 00:38:47 -0600

    first attempt, TLE

---
+++ LeetCodeTests/Hard/834 Sum of Distances in Tree/BaseCases.cs
@@ -0,0 +1,43 @@
+﻿using Leetcode.Hard._834_Sum_of_Distances_in_Tree;
+
+namespace LeetCodeTests.Hard._834_Sum_of_Distances_in_Tree;
+
+public class BaseCases {
+    [Fact]
+    public void BaseCase1() {
+        var n = 6;
+        int[][] edges = { new[] { 0, 1 }, new[] { 0, 2 }, new[] { 2, 3 }, new[] { 2, 4 }, new[] { 2, 5 } };
+        int[] expected = { 8, 12, 6, 10, 10, 10 };
+
+        Solution sut = new();
+
+        var actual = sut.SumOfDistancesInTree(n, edges);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        var n = 1;
+        int[][] edges = { Array.Empty<int>() };
+        int[] expected = { 0 };
+
+        Solution sut = new();
+        var actual = sut.SumOfDistancesInTree(n, edges);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BaseCase3() {
+        var n = 2;
+        int[][] edges = { new[] { 1, 0 } };
+        int[] expected = { 1, 1 };
+
+        Solution sut = new();
+
+        var actual = sut.SumOfDistancesInTree(n, edges);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Hard/834 Sum of Distances in Tree/Solution.cs
@@ -0,0 +1,55 @@
+﻿namespace Leetcode.Hard._834_Sum_of_Distances_in_Tree;
+
+public class Solution {
+    private int n;
+    private HashSet<int>?[] set;
+
+    public int[] SumOfDistancesInTree(int n, int[][] edges) {
+        if (n < 2)
+            return new[] { 0 };
+        set = new HashSet<int>?[n];
+        var output = new int[n];
+
+        foreach (var edge in edges) {
+            set[edge[0]] ??= new HashSet<int>();
+            set[edge[0]]!.Add(edge[1]);
+
+            set[edge[1]] ??= new HashSet<int>();
+            set[edge[1]]!.Add(edge[0]);
+        }
+
+        for (var i = 0; i < set.Length; i++) {
+            if (set[i] == null) continue;
+
+            output[i] = totalDistanceSum(i, n);
+        }
+
+        return output;
+    }
+
+    private int totalDistanceSum(int i, int n) {
+        Queue<(int, int)> q = new();
+        var cost = new int[n];
+        int currentCost;
+        int current;
+
+        q.Enqueue((i, 0));
+        while (q.Count > 0) {
+            (current, currentCost) = q.Dequeue();
+
+            if (cost[current] == 0) {
+                cost[current] = currentCost;
+                Enqueue(set[current], currentCost + 1, q);
+            }
+        }
+
+        cost[i] = 0;
+        return cost.Sum();
+    }
+
+    private static void Enqueue(IEnumerable<int>? list, int cost, Queue<ValueTuple<int, int>> q) {
+        if (list is null)
+            return;
+        foreach (var item in list) q.Enqueue((item, cost));
+    }
+}
