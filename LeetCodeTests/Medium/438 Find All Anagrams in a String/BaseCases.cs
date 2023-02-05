314709650775cc1dd67fdb5cdce1928fe9bf08ba
Karuption <pederson26@gmail.com>
2023-02-05 01:29:53 -0600

    Solved problem 438 Find All Anagrams in a String

---
+++ LeetCodeTests/Medium/438 Find All Anagrams in a String/BaseCases.cs
@@ -0,0 +1,19 @@
+﻿using Leetcode.Medium._438_Find_All_Anagrams_in_a_String;
+
+namespace LeetCodeTests.Medium;
+
+public class BaseCases {
+    [Theory]
+    [InlineData("cbaebabacd", "abc", new[] { 0, 6 })]
+    [InlineData("abab", "ab", new[] { 0, 1, 2 })]
+    [InlineData("aaaaaaaaaaaaa", "aaaaaaaaaa", new[] { 0, 1, 2, 3 })]
+    [InlineData("aa", "bb", new int[] { })]
+    [InlineData("baa", "aa", new[] { 1 })]
+    public void BaseCase(string s, string p, int[] expected) {
+        Solution sut = new();
+
+        var actual = sut.FindAnagrams(s, p);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/438 Find All Anagrams in a String/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode.Medium._438_Find_All_Anagrams_in_a_String;
+
+public class Solution {
+    public IList<int> FindAnagrams(string s, string p) {
+        List<int> anagramStart = new();
+
+        if (s.Length < p.Length)
+            return anagramStart;
+
+        var pValues = new int[26];
+        var currentValues = new int[26];
+
+        for (var i = 0; i < p.Length; i++) {
+            pValues[p[i] - 'a']++;
+            currentValues[s[i] - 'a']++;
+        }
+
+        if (currentValues.SequenceEqual(pValues))
+            anagramStart.Add(0);
+
+        for (var i = p.Length; i < s.Length; i++) {
+            currentValues[s[i - p.Length] - 'a']--;
+            currentValues[s[i] - 'a']++;
+
+            if (currentValues.SequenceEqual(pValues))
+                anagramStart.Add(i - p.Length + 1);
+        }
+
+        return anagramStart;
+    }
+}
