9b3b2a651b50aa4de79042c089c312d7fcf1ef69
Karuption <pederson26@gmail.com>
2023-03-06 00:38:55 -0600

    Binary search solution for leetcode problem 1539. Kth Missing Positive Number.
    Updated nuget packages.

--- LeetCodeTests/Easy/1539 Kth Missing Positive Number/BaseCases.cs
+++ LeetCodeTests/Easy/1539 Kth Missing Positive Number/BaseCases.cs
@@ -7,11 +7,45 @@ public class BaseCases {
 
     [Theory]
     [InlineData(new[] { 2, 3, 4, 7, 11 }, 5, 9)]
+    [InlineData(new[] { 2, 3, 4, 7, 11 }, 4, 8)]
+    [InlineData(new[] { 2, 3, 4, 7, 11 }, 1, 1)]
     [InlineData(new[] { 1, 2, 3, 4 }, 2, 6)]
+    [InlineData(new[] { 2, 4, 5}, 2, 3)]
     [InlineData(new[]{5, 6, 7, 8, 9},9,14)]
     public void BaseCase(int[] arr, int k, int expected) {
         var solution = new Solution();
         var actual = solution.FindKthPositive(arr, k);
         Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void UpperBoundTest() {
+        var solution = new Solution();
+        Span<int> arr = new int[1000];
+        for(int i = 0; i < arr.Length; i++) {
+            arr[i] = i + 1;
+        }
+
+        arr[^2]++;
+        arr[^1]++;
+        
+        var actual = solution.FindKthPositive(arr, 1);
+        
+        Assert.Equal(arr.Length-1, actual);
+    }
+    
+    [Fact]
+    public void LowerBoundTest() {
+        Span<int> arr = new int[1000];
+        
+        for(int i = 0; i < arr.Length; i++) {
+            arr[i] = i + 2;
+        }
+        
+        
+        var solution = new Solution();
+        var actual = solution.FindKthPositive(arr, 1);
+        
+        Assert.Equal(1, actual);
     }
 }
--- Leetcode/Easy/1539 Kth Missing Positive Number/Solution.cs
+++ Leetcode/Easy/1539 Kth Missing Positive Number/Solution.cs
@@ -4,17 +4,38 @@ public class Solution {
     // Find the kth positive integer that is missing from this array.
     // Constraint: 1<=k
     // Constraint: 1<=arr[i]<=1000
+    
+    //straight forward solution
+    // public int FindKthPositive(ReadOnlySpan<int> arr, int k) {
+    //     int current = 0;
+    //     int missing = 0;
+    //
+    //     foreach (var val in arr) {
+    //         while (val > ++current) {
+    //             if (k == ++missing)
+    //                 return current;
+    //         }
+    //     }
+    //
+    //     return current + (k - missing);
+    // }
+    
     public int FindKthPositive(ReadOnlySpan<int> arr, int k) {
-        int current = 0;
-        int missing = 0;
-
-        foreach (var val in arr) {
-            while (val > ++current) {
-                if (k == ++missing)
-                    return current;
-            }
+        if(k>arr[^1]-arr.Length) 
+            return k + arr.Length;
+        
+        int left = 0;
+        int right = arr.Length - 1;
+        
+        while (left <= right) {
+            int mid = left + ((right - left) >> 2);
+            if (arr[mid] - (mid + 1) < k) 
+                left = mid+1;
+            else
+                right = mid-1;
+            
         }
 
-        return current + (k - missing);
+        return k  + left;
     }
 }
