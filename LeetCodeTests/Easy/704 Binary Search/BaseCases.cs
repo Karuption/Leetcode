85f4034619867d17d877a8eb05a293b5f699439f
Karuption <pederson26@gmail.com>
2023-04-01 18:20:23 -0500

    704 Binary search completed.

---
+++ LeetCodeTests/Easy/704 Binary Search/BaseCases.cs
@@ -0,0 +1,15 @@
+﻿using Leetcode.Easy._704_Binary_Search;
+
+namespace LeetCodeTests.Easy._704_Binary_Search;
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
+    [InlineData(new[] { -1, 0, 3, 5, 9, 12 }, 2, -1)]
+    public void BaseCase(int[] arr, int target, int expected) {
+        var sut = new Solution();
+
+        var actual = sut.Search(arr, target);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Easy/704 Binary Search/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.Easy._704_Binary_Search;
+
+public class Solution {
+    public int Search(ReadOnlySpan<int> nums, int target) {
+        var l = 0;
+        var r = nums.Length - 1;
+
+        while (l <= r) {
+            var mid = l + (r - l) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] > target)
+                r = mid - 1;
+            else if (nums[mid] < target)
+                l = mid + 1;
+        }
+
+        return -1;
+    }
+}
