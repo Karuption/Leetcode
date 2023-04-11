acffabb78afa4afc36b58a64ea2f3b52d5902ef0
Karuption <pederson26@gmail.com>
2023-04-10 20:29:17 -0500

    Completed challenge 2390 Removing Stars From a String

---
+++ LeetCodeTests/Medium/2390 Removing Stars From a String/BaseCases.cs
@@ -0,0 +1,16 @@
+﻿using Leetcode.Medium._2390_Removing_Stars_From_a_String;
+
+namespace LeetCodeTests.Medium._2390_Removing_Stars_From_a_String;
+
+public class BaseCases {
+    [Theory]
+    [InlineData("leet**cod*e", "lecoe")]
+    [InlineData("erase*****", "")]
+    public void BaseCase(string input, string expected) {
+        var sut = new Solution();
+
+        var actual = sut.RemoveStars(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/2390 Removing Stars From a String/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.Medium._2390_Removing_Stars_From_a_String;
+
+public class Solution {
+    public string RemoveStars(string s) {
+        Stack<char> stack = new();
+
+        for (var i = 0; i < s.Length; i++) {
+            if (s[i] == '*')
+                stack.TryPop(out _);
+            else
+                stack.Push(s[i]);
+        }
+
+        var output = stack.ToArray();
+        Array.Reverse(output);
+
+        return new string(output);
+    }
+}
