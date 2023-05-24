d370f4ca3ae29b13b6e937be7ad9937c66523a6d
Karuption <pederson26@gmail.com>
2023-05-24 18:10:35 -0500

    684 Redundant Connection solved with UnionFind

---
+++ LeetCodeTests/Medium/684 Redundant Connection/BaseCases.cs
@@ -0,0 +1,36 @@
+﻿using Leetcode.Medium._684_Redundant_Connection;
+
+namespace LeetCodeTests.Medium._684_Redundant_Connection;
+
+public class BaseCases {
+    // Input: edges = [[1,2],[1,3],[2,3]]
+    // Output: [2,3]
+
+    [Fact]
+    public void BaseCase1() {
+        var sut = new Solution();
+        var actual = sut.FindRedundantConnection(new[] {
+            new[] { 1, 2 },
+            new[] { 1, 3 },
+            new[] { 2, 3 }
+        });
+        Assert.True(new[] { 2, 3 }.SequenceEqual(actual));
+    }
+
+    // Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+    // Output: [1,4]
+
+    [Fact]
+    public void BaseCase2() {
+        var sut = new Solution();
+        var actual = sut.FindRedundantConnection(new[] {
+            new[] { 1, 2 },
+            new[] { 2, 3 },
+            new[] { 3, 4 },
+            new[] { 1, 4 },
+            new[] { 1, 5 }
+        });
+
+        Assert.True(new[] { 1, 4 }.SequenceEqual(actual));
+    }
+}
---
+++ Leetcode/Medium/684 Redundant Connection/Solution.cs
@@ -0,0 +1,70 @@
+﻿namespace Leetcode.Medium._684_Redundant_Connection;
+
+public class Solution {
+    public int[] FindRedundantConnection(int[][] edges) {
+        var unionFind = new UnionFind(edges.Length);
+        foreach (var edge in edges) {
+            if (!unionFind.Union(edge))
+                return edge;
+        }
+
+        return null;
+    }
+}
+
+public class UnionFind {
+    private readonly int[] _elements;
+    private readonly int[] _size;
+    public int Count { get; private set; }
+
+    public UnionFind(int count) {
+        count++;
+        _elements = Enumerable.Range(0, count).ToArray();
+        _size = new int[count].Select(x => 1).ToArray();
+        Count = count;
+    }
+
+    public bool Union(int[] edge) {
+        return Union(edge[0], edge[1]);
+    }
+
+    public bool Union(int source, int destination) {
+        var sourceRoot = Find(source);
+        var destRoot = Find(destination);
+
+        if (sourceRoot == destRoot)
+            return false;
+
+        if (_size[sourceRoot] > _size[destRoot]) {
+            // dest -> source
+            _size[sourceRoot] += _size[destRoot];
+            _elements[destRoot] = _elements[sourceRoot];
+        } else {
+            // source -> dest
+            _size[destRoot] += _size[sourceRoot];
+            _elements[sourceRoot] = _elements[destRoot];
+        }
+
+        --Count;
+        return true;
+    }
+
+    public int Find(int id) {
+        if (id == _elements[id])
+            return id;
+
+        // Route compression
+        // Find root
+        var current = _elements[id];
+        while (current != _elements[current])
+            current = _elements[current];
+
+        // Set everything in the path to root, to the root
+        var root = current;
+        current = id;
+        while (current != root)
+            (_elements[current], current) = (root, _elements[current]);
+
+        return root;
+    }
+}
