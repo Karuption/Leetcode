f67d5b5000ffd4cc992a98a92ba8a9f73805d2e2
Karuption <pederson26@gmail.com>
2023-05-25 15:03:35 -0500

    207. Course Schedule solved with topological sort.

---
+++ LeetCodeTests/Medium/207 Course Schedule/BaseCases.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode.Medium._207_Course_Schedule;
+
+namespace LeetCodeTests.Medium._207_Course_Schedule;
+
+public class BaseCases {
+    //There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+    //    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    //Return true if you can finish all courses. Otherwise, return false.
+
+    //Input: numCourses = 2, prerequisites = [[1,0]]
+    //Output: true
+
+    //Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    //Output: false
+
+    private readonly int[][] case1 = { new[] { 1, 0 } };
+    private readonly int[][] case2 = { new[] { 1, 0 }, new[] { 0, 1 } };
+
+    [Fact]
+    public void BaseCase1() {
+        Solution sut = new();
+
+        var actual = sut.CanFinish(2, case1);
+
+        Assert.Equal(true, actual);
+    }
+
+    [Fact]
+    public void BaseCase2() {
+        Solution sut = new();
+
+        var actual = sut.CanFinish(2, case2);
+
+        Assert.Equal(false, actual);
+    }
+}
---
+++ Leetcode/Medium/207 Course Schedule/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Leetcode.Medium._207_Course_Schedule;
+
+public class Solution {
+    private HashSet<int>[] mapping;
+    private bool[] visited;
+
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        mapping = Enumerable.Range(0, numCourses).Select(x => new HashSet<int>()).ToArray();
+        visited = new bool[numCourses];
+
+        foreach (var edge in prerequisites)
+            // [a,b] you must take b to take a
+            mapping[edge[0]].Add(edge[1]);
+
+        while (TakeEligibleClasses() > 0)
+            ;
+
+        foreach (HashSet<int> edge in mapping) {
+            if (edge.Count > 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    private int TakeEligibleClasses() {
+        var count = 0;
+        for (var i = 0; i < mapping.Length; i++) {
+            // Get a class with 0 in degree
+            if (mapping[i].Count == 0 && visited[i] == false) {
+                visited[i] = true;
+                count += RemovePrereq(i);
+            }
+        }
+
+        return count;
+    }
+
+    private int RemovePrereq(int i) {
+        var count = 0;
+
+        for (var j = 0; j < mapping.Length; j++) {
+            if (mapping[j].Contains(i)) {
+                mapping[j].Remove(i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
