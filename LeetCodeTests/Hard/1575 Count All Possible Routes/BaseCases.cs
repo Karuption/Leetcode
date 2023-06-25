c17a1af784d4c6317e10dcd991707eddbb48e0dc
Karuption <pederson26@gmail.com>
2023-06-25 02:10:09 -0500

    Completed Hard 1575 Count All Possible Routes Completed
    Updated .gitignore
    Used Dynamic Programming to find the solution. Memoizing the results for reasonable execution time.

---
+++ LeetCodeTests/Hard/1575 Count All Possible Routes/BaseCases.cs
@@ -0,0 +1,19 @@
+using Leetcode.Hard._1575_Count_All_Possible_Routes;
+
+namespace LeetCodeTests.Hard._1575_Count_All_Possible_Routes; 
+
+public class BaseCases {
+    [Theory]
+    [InlineData(new[] { 2,3,6,8,4 }, 1, 3, 5, 4)]
+    [InlineData(new [] {4,3,1}, 1, 0, 6, 5)]
+    [InlineData(new int[]{5,2,1}, 0, 2, 3, 0)]
+    [InlineData(new int[]{1,2,3}, 0, 2, 40, 615088286)]
+
+    public void BaseCase(int[] locations, int start, int finish, int fuel, int expected) {
+        var sut = new Solution();
+
+        var actual = sut.CountRoutes(locations, start, finish, fuel);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Hard/1575 Count All Possible Routes/Solution.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Leetcode.Hard._1575_Count_All_Possible_Routes; 
+
+public class Solution {
+    private Dictionary<int, Dictionary<int, int>> Memo = new();
+    private int[,] _dp = null!;
+    private int[] _locations = null!;
+
+    public int CountRoutes(int[] locations, int start, int finish, int fuel) {
+        _locations = locations;
+        _dp = new int[locations.Length, fuel + 1];
+        for (int i = 0; i < locations.Length; i++) {
+            for (int j = 0; j < fuel+1; j++) {
+                _dp[i,j] = -1;
+            }
+        }
+        return countRoutes(start, finish, fuel);
+    }
+
+    private int countRoutes(int current, int finish, int fuel) {
+        //if we can't get to the finish
+        if (fuel < 0)
+            return 0;
+        
+        if (_dp[current, fuel] != -1)
+            return _dp[current,fuel];
+        
+        var count = 0;
+        if (current == finish)
+            count++;
+        
+        //if there is gas to go to other places
+        for (var i = 0; i < _locations.Length; i++) {
+            if(current == i)
+                continue;
+
+            var fuelLeft = fuel - GetDistance(current, i);
+            count = (count + countRoutes(i, finish, fuelLeft) )% 1_000_000_007;
+        }
+
+        return _dp[current, fuel] = count;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private int GetDistance(int from, int to) {
+        return Math.Abs(_locations[from] - _locations[to]);
+    }
+}
