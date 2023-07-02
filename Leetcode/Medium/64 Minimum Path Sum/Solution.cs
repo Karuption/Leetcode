dd9d82602f679f4883510bc7a5dca10ee031e465
Karuption <pederson26@gmail.com>
2023-07-02 00:01:43 -0500

    Refactor Medium 64. Minimum Path Sum
    Reduced the space complexity from O(n*m) to O(n)

--- Leetcode/Medium/64 Minimum Path Sum/Solution.cs
+++ Leetcode/Medium/64 Minimum Path Sum/Solution.cs
@@ -4,7 +4,7 @@ public class Solution {
     private int[][] _memo;
 
     public int MinPathSum(int[][] grid) {
-        _memo = Enumerable.Range(0,grid.Length).Select(_=>Enumerable.Range(0,grid[0].Length).Select(_=>int.MaxValue).ToArray()).ToArray();
+        _memo = Enumerable.Range(0,2).Select(_=>Enumerable.Range(0,grid[0].Length).Select(_=>int.MaxValue).ToArray()).ToArray();
 
         _memo[0][0] = grid[0][0];
         for (int i = 1; i < grid[0].Length; i++) {
@@ -12,13 +12,13 @@ public class Solution {
         }
         
         for (int i = 1; i < grid.Length; i++) {
-            _memo[i][0] = _memo[i - 1][0] + grid[i][0];
+            _memo[i&1][0] = _memo[(i - 1)&1][0] + grid[i][0];
             for (int j = 1; j < grid[0].Length; j++) {
-                _memo[i][j] = grid[i][j] + Math.Min(_memo[(i - 1)][j], _memo[i][j - 1]);
+                _memo[i&1][j] = grid[i][j] + Math.Min(_memo[(i - 1)&1][j], _memo[i&1][j - 1]);
             }
         }
 
-        return _memo[(grid.Length-1)][^1];
+        return _memo[(grid.Length-1)&1][^1];
     }
     public int MinPathSum_DFS(int[][] grid) {
         _memo = Enumerable.Range(0,grid.Length).Select(_=>new int[grid[0].Length]).ToArray();
