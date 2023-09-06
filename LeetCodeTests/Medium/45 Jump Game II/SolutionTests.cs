94d04039e1759c37660f1d2417b0e0eafc461e06
Karuption <pederson26@gmail.com>
2023-09-06 16:17:04 -0500

    Completed Medium 45. Jump Game II I used an algebraic solution for O(n) complexity and O(1) space.

---
+++ LeetCodeTests/Medium/45 Jump Game II/SolutionTests.cs
@@ -0,0 +1,17 @@
+using Leetcode.Medium._45_Jump_Game_II;
+
+namespace LeetCodeTests.Medium._45_Jump_Game_II; 
+
+public class SolutionTests {
+
+    [InlineData(new[] { 2, 3, 1, 1, 4 }, 2)]
+    [InlineData(new[] { 2, 3, 0, 1, 4 }, 2)]
+    [Theory]
+    public void BaseCases(int[] nums, int expected) {
+
+        var sut = new Solution();
+        var actual = sut.Jump(nums);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/45 Jump Game II/Solution.cs
@@ -0,0 +1,22 @@
+namespace Leetcode.Medium._45_Jump_Game_II;
+
+public class Solution {
+    public int Jump(int[] nums) {
+        var maxIndex = 0;
+        var currentEnd = maxIndex;
+        var jumps = 0;
+        
+        for (var i = 0; i < nums.Length-1; i++) {
+            maxIndex = Math.Max(nums[i] + i, maxIndex);
+            
+            // reached the furthest I can, have to jump
+            // jump to the square that gets me the furthest (max)
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = maxIndex;
+            }
+        }
+        
+        return jumps;
+    }
+}
