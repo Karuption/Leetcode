4a530548e039ec15e5de4512b1406a7f34eaef62
Karuption <pederson26@gmail.com>
2023-09-08 15:49:29 -0500

    Completed Medium 134. Gas Station
    An O(n) complexity O(1) space solution.

--- Leetcode/Medium/134 Gas Station/Solution.cs
+++ Leetcode/Medium/134 Gas Station/Solution.cs
@@ -2,23 +2,20 @@ namespace Leetcode.Medium._134_Gas_Station;
 
 public class Solution {
     public int CanCompleteCircuit(int[] gas, int[] cost) {
+        var runningSum = 0;
+        var startIndex = 0;
+        var current = 0;
+        
         for(var i = 0; i < gas.Length; i++) {
-            var gasAmount = gas[i] - cost[i];
-            if(gasAmount<0){
-                continue;
+            current += gas[i] - cost[i];
+            runningSum += gas[i]-cost[i];
+            
+            if (current < 0) {
+                startIndex = i+1;
+                current = 0;
             }
-
-            for(var j = (i+1)%gas.Length; j != i; j=(j+1)%gas.Length) {
-                gasAmount += gas[j];
-                gasAmount -= cost[j];
-                if(gasAmount < 0)
-                    break;
-            }
-
-            if(gasAmount >= 0)
-                return i;
         }
 
-        return -1;
+        return runningSum >= 0? startIndex : -1;
     }
 }
