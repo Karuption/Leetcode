693d428d9448847c417233a1b07b7b85345a4ed5
Karuption <pederson26@gmail.com>
2023-09-12 17:52:06 -0500

    Completed Medium 1647. Minimum Deletions to Make Character Frequencies Unique
    Used an intuative method. Time Complexity O(n+m) Space Complexity O(1).
    
    n is the string length
    m is the number of times that are needed to decrament each frequency count in the while loop. This can become a non-trivial amount in cases some cases. For example, when each char has the same frequency.
    
    1. get the frequencies
    2. Try and insert all of them into a hash set.
    3. if the hasthe key already, decrament the frequency, incrament the altercation count and try step 2 again.

---
+++ LeetCodeTests/Medium/1647 Minimum Deletions to Make Character Frequencies Unique/SolutionTests.cs
@@ -0,0 +1,16 @@
+using Leetcode.Medium._1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique;
+namespace LeetCodeTests.Medium._1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique; 
+
+public class SolutionTests {
+    [Theory]
+    [InlineData("aab", 0)]
+    [InlineData("aaabbbcc", 2)]
+    [InlineData("ceabaacb", 2)]
+    public void BaseCases(string input, int expected) {
+        var sut = new Solution();
+        var actual = sut.MinDeletions(input);
+        
+        Assert.Equal(expected, actual);
+    }
+
+}
---
+++ Leetcode/Medium/1647 Minimum Deletions to Make Character Frequencies Unique/Solution.cs
@@ -0,0 +1,21 @@
+namespace Leetcode.Medium._1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique; 
+
+public class Solution {
+    public int MinDeletions(string s) {
+        Span<int> freq = stackalloc int[26];
+
+        foreach(var c in s)
+            freq[c-'a']++;
+
+        int alterations= 0;
+        HashSet<int> set = new(26);
+        for(int i = 0; i < freq.Length; i++) {
+            while(!set.Add(freq[i]) && freq[i] != 0) {
+                freq[i]--;
+                alterations++;
+            }
+        }
+
+        return alterations;
+    }
+}
