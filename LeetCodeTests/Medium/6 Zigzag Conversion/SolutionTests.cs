ec0d8149ee2cc76a1f61f5e26311d6e96b1d1823
Karuption <pederson26@gmail.com>
2023-09-13 16:36:06 -0500

    Completed Medium 6. Zigzag Conversion
    Using a straight forward approach.
    
    1. create an array of lists as long as the number of rows
    2. add data to the end of the list that is chosen by array index oscalation.
    3. loop through the lists and build a string to output.

---
+++ LeetCodeTests/Medium/6 Zigzag Conversion/SolutionTests.cs
@@ -0,0 +1,15 @@
+using Leetcode.Medium._6_Zigzag_Conversion;
+namespace LeetCodeTests.Medium._6_Zigzag_Conversion;
+
+public class SolutionTests {
+    [Theory]
+    [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+    [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+    [InlineData("A", 1, "A")]
+    public void BaseCases(string s, int numRows, string expected) {
+        var sut = new Solution();
+        var actual = sut.Convert(s, numRows);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Medium/6 Zigzag Conversion/Solution.cs
@@ -0,0 +1,41 @@
+using System.Text;
+
+namespace Leetcode.Medium._6_Zigzag_Conversion; 
+
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if(numRows == 1)
+            return s;
+            
+        List<char>[] lc = Enumerable.Range(0,numRows).Select(_=>new List<char>()).ToArray();
+    
+        var decending = true;
+        int index = 0;
+        
+        for(int i = 0; i < s.Length; i++) {
+            lc[index].Add(s[i]);
+                 
+            if(index == 0 || index == numRows-1) {
+                decending = !decending;
+                if(decending)
+                    index--;
+                else
+                    index++;
+            } else {
+                if(decending)
+                    index--;
+                else
+                    index++;
+            }
+        }
+        
+        StringBuilder sb = new();
+        foreach(var l in lc) {
+            foreach(var c in l) {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
