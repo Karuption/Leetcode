a3f7953c3d8c4aa53b9c0c411e26cf1f8fe925ba
Karuption <pederson26@gmail.com>
2023-09-28 18:35:52 -0500

    Completed Medium 71. Simplify Path

---
+++ LeetCodeTests/Medium/71 Simplify Path/SolutionTests.cs
@@ -0,0 +1,16 @@
+using Leetcode.Medium._71_Simplify_Path;
+namespace LeetCodeTests.Medium._71_Simplify_Path; 
+
+public class SolutionTests {
+    [Theory]
+    [InlineData("/home/", "/home")]
+    [InlineData("/../", "/")]
+    [InlineData("/home//foo/", "/home/foo")]
+    public void BaseCases(string input, string expected) {
+        var sut = new Solution();
+        var actual = sut.SimplifyPath(input);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+}
---
+++ Leetcode/Medium/71 Simplify Path/Solution.cs
@@ -0,0 +1,25 @@
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Leetcode.Medium._71_Simplify_Path; 
+
+public class Solution {
+    public string SimplifyPath(string path) {
+        var dirs = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        Stack<string> stack = new(dirs.Length);
+
+
+        foreach (var dir in dirs) {
+            if (dir == ".")
+                continue;
+
+            if (dir == "..") { 
+                stack.TryPop(out _);
+                continue;
+            }
+            stack.Push(dir);
+        }
+
+        return "/" + string.Join("/", stack.Reverse());
+    }
+}
