24572014bc97f4275bcb153dbcbb97dbec74bb41
Karuption <pederson26@gmail.com>
2023-09-29 20:30:40 -0500

    Completed Medium 7. Reverse Integer

---
+++ Leetcode/Medium/7 Reverse Integer/Solution.cs
@@ -0,0 +1,22 @@
+namespace Leetcode.Medium._7_Reverse_Integer; 
+
+public class Solution {
+    public int Reverse(int x) {
+        var rev = 0;
+
+        try {
+            var absx = Math.Abs(x);
+            checked {
+                while(absx > 0) {
+                    rev = (rev*10) + absx%10;
+                    absx /= 10;
+                }
+                rev *= Math.Sign(x);
+            }
+        } catch {
+            return 0;
+        }
+
+        return rev;
+    }
+}
