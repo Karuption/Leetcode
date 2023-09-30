bbf4bbc4ce308c5479af7855cf7c2880af3845a0
Karuption <pederson26@gmail.com>
2023-09-30 18:45:52 -0500

    ListNode bug and ToList feature
    Added a ToList extension method on ListNodes. This will make unit testing easier by allowing a .ToList().ToArray(). Test performance is not a big issue. If it becomes one, we can scan the list and create a perfectly sized array/list.
    
    Int[] -> ToListNode had a bug where there would be empty Listnode cells in the output.

--- LeetCodeTests/Helpers/LinkedListArrayConverter.cs
+++ LeetCodeTests/Helpers/LinkedListArrayConverter.cs
@@ -2,19 +2,28 @@
 
 namespace LeetCodeTests.Helpers;
 
+#nullable disable
 public static class LinkedListArrayConverter {
+    public static List<int> ToList(this ListNode linkedList) {
+        var list = new List<int>();
+        var current = linkedList;
+        
+        while (current is not null) {
+            list.Add(current.val);
+            current = current.next;
+        }
+
+        return list;
+    }
     public static ListNode ToListNode(this int[] nums) {
         var head = new ListNode();
         var current = head;
 
-        for (var i = 0; i < nums.Length; i++) {
-            current.val = nums[i];
-            current.next = new ListNode();
-            current = current.next;
-        }
+        for (var i = 0; i < nums.Length; i++)
+            current = current.next = new ListNode(nums[i]);
 
         current.next = null;
 
-        return head;
+        return head.next;
     }
 }
